6f23ebfd32beb396b2ffb05f811ee150adbad7e7
wolfee <wolfezzz47@gmail.com>
2026-01-18 15:44:23 +0200

    simple

--- simplestr.py
+++ simplestr.py
@@ -4,3 +4,36 @@ print(mystr)
 name = "Pedro"
 
 print (name[2])
+
+##name[0] = "c"
+##print("name = ", name)
+
+name = "Carlos"
+print("name = ", name)
+
+print("name[2:4] = ", name[2:4])
+print("name[0:3] = ", name[0:3])
+
+name = """Ana Maria 
+Gonzalez"""
+print("name = ", name)
+
+test_str = "  Hello World  " + name
+print("test_str = ", test_str)
+
+#for
+print( "--iterachia--:" )
+name = "Pedro"
+for char in name:
+    print(char)
+
+print("cu" in name)
+print("ka" in name)
+print("ed" in name)
+
+print("---len()---")
+print("len(name) = ", len(name))
+
+hello = "Hello"
+print("hello = ", hello)
+print("reverse hello = ", hello[::-1])
